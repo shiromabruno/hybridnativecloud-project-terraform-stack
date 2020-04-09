3f3315eeb4511ca1010b14ce71503530c10da940
Ubuntu <ubuntu@ip-172-31-91-61.ec2.internal>
2020-04-09 01:16:13 +0000

    atualizacao do grupo e RM

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackathon-fiap-34scj-rm334070"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
